7f130529ce181ebe51b90326e587d2b2db6b13c6
Mharr <Timido@Timido.local>
2015-04-07 21:09:05 -0700

    Schedule.java written - small updates to Record.java -not quite working, but almost

--- app/src/main/java/com/mm90849491/sleepguard/Record.java
+++ app/src/main/java/com/mm90849491/sleepguard/Record.java
@@ -10,31 +10,66 @@ import java.io.IOException;
 import android.os.Environment;
 import android.util.Log;
 
-/**
- * store the information about the recorded file
- * shared by UserInterface, InputOutput, and DataAnalysing
+/** Record class.
+ *      Stores the information about the recorded file
+ *  @version 0.4.0
+ *  @author M.Harrison
+ *  @see com.mm90849491.sleepguard.Record
  */
-public class Record {
 
+public class Record {
+    /* ------------ beginning of constant variables ------------ */
+    static private int[] frequencyAvailable = {0, 8000, 32000, 44100, 48000};
+    static private int frUsed = frequencyAvailable[2];
+    /* ------------------ end of constant variables ------------ */
+
+    /* ------------ beginning of instance variables ------------ */
     private AudioRecord re = null;
-    private boolean isRecording; // in my example program I used 2 rather than 1
+    private boolean isRecording;
     private Thread thr = null;
     private FileOutputStream out = null;
     private int bufferSize;
     private String _targetPath;
     private String _targetFilename;
     private File _targetFile;
+    /* ------------------ end of instance variables ------------ */
 
+    /* ------------ beginning of getter/setter methods --------- */
+    /** Gets path to WAV file: _targetPath.
+     * @return String
+     */
     public String targetPath() { return _targetPath; }
-    public void targetPath(String input) { this._targetPath = input; }
-    public String targetFilename() { return _targetFilename; }
-    public void targetFilename(String input) { this._targetFilename = input; }
-    public File targetFile() { return _targetFile; }
-    public void targetFile(File input) { this._targetFile = input; }
 
-    public Record(/*File DIRECTORY, String NAME*/) {
-        //this.targetDirectory(DIRECTORY);
-        //this.targetName(NAME);
+    /** Sets path to WAV file: _targetPath.
+     * @param input String
+     */
+    public void targetPath(String input) { this._targetPath = input; }
+
+    /** Gets filename of WAV file: _targetFilename.
+     * @return String
+     */
+    public String targetFilename() { return _targetFilename; }
+
+    /** Sets path to WAV file: _targetFilename.
+     * @param input String
+     */
+    public void targetFilename(String input) { this._targetFilename = input; }
+
+    /** Gets main file: _targetFile.
+     * @return java.io.File
+     */
+    public File targetFile() { return _targetFile; }
+
+    /** Sets main file: _targetFile.
+     * @param input java.io.File
+     */
+    public void targetFile(File input) { this._targetFile = input; }
+    /* -------------------end of getter/setter methods --------- */
+
+    /* ------------ beginning of constructors ------------------ */
+    /** Constructs the Record instance.
+     */
+    public Record() {
         bufferSize = AudioRecord.getMinBufferSize(
                 frUsed,
                 AudioFormat.CHANNEL_IN_MONO,
@@ -47,7 +82,11 @@ public class Record {
                 AudioFormat.ENCODING_PCM_16BIT,
                 bufferSize);
     }
+    /* ------------------ end of constructors ------------------ */
 
+    /* ------------ beginning of public methods ---------------- */
+    /** Starts the recording process.
+     */
     public void startRe() {
         re.startRecording();
         isRecording = true;
@@ -60,6 +99,8 @@ public class Record {
         thr.start();
     }
 
+    /** Stops the recording process.
+     */
     public void stopRe() {
         isRecording = false;
         re.stop();
@@ -67,9 +108,14 @@ public class Record {
         re = null;
         thr = null;
     }
+    /* ------------------ end of public methods ---------------- */
 
-    // found at http://stackoverflow.com/questions/8499042/android-audiorecord-example
-    // unchanged
+    /* ------------ beginning of private methods --------------- */
+    /** Translates an array of Shorts to an array of Bytes.
+     * Found at http://stackoverflow.com/questions/8499042/android-audiorecord-example; Unchanged.
+     * @param sData Array of Shorts.
+     * @return Array of Bytes.
+     */
     private byte[] short2byte(short[] sData) {
         int shortArraySize = sData.length;
         byte[] bytes = new byte[shortArraySize * 2];
@@ -81,11 +127,12 @@ public class Record {
         return bytes;
     }
 
-    // found at http://stackoverflow.com/questions/8499042/android-audiorecord-example (same as above)
-    // changed somewhat
+    /** Writes the audio information to a WAV file (including the header).
+     * Found at (same URL as above); changed extensively.
+     */
     private void writeAudioDataToFile() {
         targetPath(Environment.getExternalStorageDirectory().getPath());
-        targetFilename("/sleep.wav"); // I used PCM because it's a pre-stripped Wav file, I could make it a wav file too
+        targetFilename("/sleep.wav");
         short sData[] = new short[bufferSize];
 
         try {
@@ -95,35 +142,31 @@ public class Record {
             e.printStackTrace();
         }
 
-        // header
         try {
             byte[] header = new byte[44];
             int byteRate = frUsed * 16 * 1 / 8;
 
-            int duration = 0; // should be calculated from duration of the scheduling
-            int totalAudioLength = frUsed * duration / 1000;
-
-            header[0] = 'R';  // RIFF/WAVE header
+            header[0] = 'R';
             header[1] = 'I';
             header[2] = 'F';
             header[3] = 'F';
-            header[4] = (byte) ((totalAudioLength+36) & 0xff);
-            header[5] = (byte) (((totalAudioLength+36) >> 8) & 0xff);
-            header[6] = (byte) (((totalAudioLength+36) >> 16) & 0xff);
-            header[7] = (byte) (((totalAudioLength+36) >> 24)& 0xff);
+            header[4] = 0; // still needs to be calculated after
+            header[5] = 0;
+            header[6] = 0;
+            header[7] = 0;
             header[8] = 'W';
             header[9] = 'A';
             header[10] = 'V';
             header[11] = 'E';
-            header[12] = 'f';  // 'fmt ' chunk
+            header[12] = 'f';
             header[13] = 'm';
             header[14] = 't';
             header[15] = ' ';
-            header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+            header[16] = 16;
             header[17] = 0;
             header[18] = 0;
             header[19] = 0;
-            header[20] = 1;  // format = 1
+            header[20] = 1;
             header[21] = 0;
             header[22] = (byte) AudioFormat.CHANNEL_IN_MONO;
             header[23] = 0;
@@ -137,31 +180,26 @@ public class Record {
             header[31] = (byte) ((byteRate >> 24) & 0xff);
             header[32] = (byte) (AudioFormat.CHANNEL_IN_MONO * AudioFormat.ENCODING_PCM_16BIT / 8);  // block align
             header[33] = 0;
-            header[34] = 2;  // bits per sample (channels * bitspersample / 8)
+            header[34] = 2;
             header[35] = 0;
             header[36] = 'd';
             header[37] = 'a';
             header[38] = 't';
             header[39] = 'a';
-            header[40] = (byte) (totalAudioLength & 0xff);
-            header[41] = (byte) ((totalAudioLength >> 8) & 0xff);
-            header[42] = (byte) ((totalAudioLength >> 16) & 0xff);
-            header[43] = (byte) ((totalAudioLength >> 24) & 0xff);
-
+            header[40] = 0; // still needs to be calculated
+            header[41] = 0;
+            header[42] = 0;
+            header[43] = 0;
             out.write(header, 0, 44);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         while (isRecording) {
-            // gets the voice output from microphone to byte format
-
             re.read(sData, 0, bufferSize);
             try {
-                // // writes the data to file from buffer
-                // // stores the voice buffer
                 byte bData[] = short2byte(sData);
-                out.write(bData, 0, bufferSize); // rather than writing to a file here, maybe it could be analyzed here on the fly (if that's more difficult, that's fine
+                out.write(bData, 0, bufferSize);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -173,24 +211,27 @@ public class Record {
             e.printStackTrace();
         }
     }
+    /* ------------------ end of private methods --------------- */
 
-    // added 8000 - in my readings supposedly that's what the computer uses
-    static private int[] frequencyAvailable = {0, 8000, 32000, 44100, 48000};
-    static private int frUsed = frequencyAvailable[2];
 
+    /*
+     * Meng, these are the variables you wrote -- I won't delete them until later when we sure
+     * that we won't need them or whatever.
+     */
+    /*
     private File _targetDirectory;
     private String _targetName;
     private String _targetExtension;
-    /* char == unsigned int */
+    /* char == unsigned int /
     private char _bitRate = 32; // ??
-    /* Default(device recording setting) 32000 44100 48000 */
+    /* Default(device recording setting) 32000 44100 48000 /
     private byte _frequencyLevel = 0;
     private Result _result;
 
     /**
      *
      * @return File _targetDirectory
-     */
+     /
     public File targetDirectory() {
         return _targetDirectory;
     }
@@ -198,7 +239,7 @@ public class Record {
     /**
      *
      * @param :File _targetDirectory
-     */
+     /
     public void targetDirectory(File _targetDirectory) {
         this._targetDirectory = _targetDirectory;
     }
@@ -236,8 +277,9 @@ public class Record {
     /**
      *
      * @return int sampling frequency
-     */
+     /
     public int frequency() {
         return Record.frequencyAvailable[this.frequencyLevel()];
     }
+    */
 }
--- app/src/main/java/com/mm90849491/sleepguard/Schedule.java
+++ app/src/main/java/com/mm90849491/sleepguard/Schedule.java
@@ -1,12 +1,250 @@
 package com.mm90849491.sleepguard;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+/* To Alex:
+ * The schedule class is close, but it's still buggy.
+ * I wanted to get this up for you so you can get the GUI
+ * working with it.  The Schedule(int, int, int, int) constructor
+ * needs to be called with the information where the four ints are
+ * as follows:
+ *      int hourIn              - the hour of the time to schedule the recording
+ *      int minuteIn            - the minute of the time to schedule the recording
+ *      int durationHoursIn     - the hour of how long the recording will take
+ *      int durationMinutesIn   - the minute of how long the recording will take
+ * If you prefer different inputs/outputs, just let me know and I'll
+ * change it to however you'd like.
+ *
+ * Cheers,
+ * Matt
+ */
 
-/**
+import android.os.Handler;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Calendar;
+import android.util.Log;
+
+/** Schedule class.
+ *      Schedules the instance of the Record class.
+ *  @version 0.4.0
+ *  @author M.Harrison
+ *  @see com.mm90849491.sleepguard.Schedule
  */
 public class Schedule {
-    /* UTC: [-1200: 30: +1400] */
+
+    /* ------------ beginning of constant variables ------------ */
+    static private int HOURS_PER_DAY = 24;
+    static private int MINUTES_PER_HOUR = 60;
+    static private int SECONDS_PER_MINUTE = 60;
+    static private int MILLIS_PER_SECOND = 1000;
+    /* ------------------ end of constant variables ------------ */
+
+    /* ------------ beginning of instance variables ------------ */
+    private final Handler handler = new Handler();
+    private Timer timer = null;
+    private TimerTask timerTask = null;
+    private Record recorder = null;
+    private boolean isRecording;
+    private int _hour;
+    private int _minute;
+    private int _currentHour;
+    private int _currentMinute;
+    private int _durationHours;
+    private int _durationMinutes;
+    private int _timeUntilStart;
+    private int _recordingDuration;
+    /* ------------------ end of instance variables ------------ */
+
+    /* ------------ beginning of getter/setter methods --------- */
+    /** Gets hour to start recording in 24-hour-clock: _hour.
+     * @return int
+     */
+    public int hour() { return _hour; }
+
+    /** Sets hour to start recording in 24-hour-clock: _hour.
+     * @param input int
+     */
+    public void hour(int input) { this._hour = input; }
+
+    /** Gets minute to start recording: _minute.
+     * @return int
+     */
+    public int minute() { return _minute; }
+
+    /** Sets minute to start recording: _minute.
+     * @param input int
+     */
+    public void minute(int input) { this._minute = input; }
+
+    /** Gets current time's hour: _currentHour.
+     * @return int
+     */
+    public int currentHour() { return _currentHour; }
+
+    /** Sets current time's hour: _currentHour.
+     * @param input int
+     */
+    public void currentHour(int input) { this._currentHour = input; }
+
+    /** Gets current time's minute: _currentMinute.
+     * @return int
+     */
+    public int currentMinute() { return _currentMinute; }
+
+    /** Sets current time's minute: _currentMinute.
+     * @param input int
+     */
+    public void currentMinute(int input) { this._currentMinute = input; }
+
+    /** Gets amount of hours of the recording: _durationHours.
+     * @return int
+     */
+    public int durationHours() { return _durationHours; }
+
+    /** Sets amount of hours of the recording: _durationHours.
+     * @param input int
+     */
+    public void durationHours(int input) { this._durationHours = input; }
+
+    /** Gets remainder of minutes of the recording: _durationMinutes.
+     * @return int
+     */
+    public int durationMinutes() { return _durationMinutes; }
+
+    /** Sets remainder of minutes of the recording: _durationMinutes.
+     * @param input int
+     */
+    public void durationMinutes(int input) { this._durationMinutes = input; }
+
+    /** Gets time until the recording starts in milliseconds: _timeUntilStart.
+     * @return int
+     */
+    public int timeUntilStart() { return _timeUntilStart; }
+
+    /** Sets time until the recording starts in milliseconds: _timeUntilStart.
+     * @param input int
+     */
+    public void timeUntilStart(int input) { this._timeUntilStart = input; }
+
+    /** Gets the duration of the recording in milliseconds: _recordingDuration.
+     * @return int
+     */
+    public int recordingDuration() { return _recordingDuration; }
+
+    /** Sets the duration of the recording in milliseconds: _recordingDuration.
+     * @param input int
+     */
+    public void recordingDuration(int input) { this._recordingDuration = input; }
+    /* -------------------end of getter/setter methods --------- */
+
+    /* ------------ beginning of constructors ------------------ */
+    /** Constructs the Schedule instance.
+     * @param hourIn (int) The hour that the recording should start in 24-hour time.
+     * @param minuteIn (int) The minute that the recording should start in 24-hour time.
+     * @param durationHoursIn (int) The number of hours of the intended duration of the recording.
+     * @param durationMinutesIn (int) The remainder of minutes of the intended duration of the recording.
+     */
+    public Schedule(int hourIn, int minuteIn, int durationHoursIn, int durationMinutesIn) {
+        if (areValidTimes()) {
+            recorder = new Record();
+            isRecording = false;
+            hour(hourIn);
+            minute(minuteIn);
+            currentHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+            currentMinute(Calendar.getInstance().get(Calendar.MINUTE));
+            durationHours(durationHoursIn);
+            durationMinutes(durationMinutesIn);
+            calculateTimes();
+            startTi();
+            Log.e("tagtag", "1");
+        }
+    }
+    /* ------------------ end of constructors ------------------ */
+
+    /* ------------ beginning of public methods ---------------- */
+
+    /** Checks that the input hour and minute are in the 24-hour clock.
+     * @return true if both the hour and minute are valid.
+     */
+    public boolean areValidTimes() {
+        return true;
+    }
+
+    /** The main public method that starts the schedule (and by extension the recording).
+     */
+    public void startTi() {
+        Log.e("tagtag", "2");
+        timer = new Timer();
+
+        if (!isRecording) {
+            initializeTimerTaskStart();
+
+            timer.schedule(timerTask, timeUntilStart(), recordingDuration());
+        }
+    }
+
+    /** The main public method that stops the scheduled task and performs requisite resets.
+     */
+    public void stopTi() {
+        timer = null;
+        recorder.stopRe();
+        recorder = null;
+        isRecording = false;
+    }
+    /* ------------------ end of public methods ---------------- */
+
+    /* ------------ beginning of private methods --------------- */
+    /** Handler that starts or stops the scheduled recording as needed.
+     */
+    private void initializeTimerTaskStart() {
+        timerTask = new TimerTask() {
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        if (!isRecording) {
+                            isRecording = true;
+                            Log.e("tagtag", "Started at a time.");
+                            recorder.startRe();
+                        } else {
+                            recorder.stopRe();
+                            recorder = null;
+                            isRecording = false;
+                            timer.cancel();
+                            timer.purge();
+                            Log.e("tagtag", "Stopped a minute later.");
+                        }
+                    }
+                });
+            }
+        };
+    }
+    /** Given the integer inputs, calculates in milliseconds the amount of time before the
+     * recording should start and in milliseconds the intended duration fo the recording.
+     */
+    private void calculateTimes() {
+        int addMillisForDuration = 0, addMillisForStart = 0;
+
+        if (currentHour() <= hour()) {
+            addMillisForStart += (hour() - currentHour()) * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MILLIS_PER_SECOND;
+        } else {
+            addMillisForStart += (HOURS_PER_DAY - currentHour() + hour()) * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MILLIS_PER_SECOND;
+        }
+
+        if (currentMinute() <= minute()) {
+            addMillisForStart += (minute() - currentMinute()) * SECONDS_PER_MINUTE * MILLIS_PER_SECOND;
+        } else {
+            addMillisForStart += (MINUTES_PER_HOUR - currentMinute() + minute()) * SECONDS_PER_MINUTE * MILLIS_PER_SECOND;
+        }
+        timeUntilStart(addMillisForStart);
+
+        addMillisForDuration += durationHours() * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MILLIS_PER_SECOND;
+        addMillisForDuration += durationMinutes() * SECONDS_PER_MINUTE * MILLIS_PER_SECOND;
+        recordingDuration(addMillisForDuration);
+    }
+
+    /*
+    * Like in Record - I'll leave these available in case they're needed.
+     */
+    /* UTC: [-1200: 30: +1400] /
     private int timeZone = -800;
     private GregorianCalendar startTime;
     private GregorianCalendar endTime;
@@ -40,7 +278,7 @@ public class Schedule {
      *
      * @param startTime
      * @param duration HH * 100 + MM
-     */
+     /
     public void setStartTime(GregorianCalendar startTime, int duration) {
         this.setStartTime(startTime);
         this.endTime.add(Calendar.MINUTE, -(duration % 100));
@@ -59,7 +297,7 @@ public class Schedule {
      *
      * @param endTime
      * @param duration HH * 100 + MM
-     */
+     /
     public void setEndTime(GregorianCalendar endTime, int duration) {
         this.setEndTime(endTime);
         this.endTime.add(Calendar.MINUTE, duration % 100);
@@ -81,4 +319,5 @@ public class Schedule {
     public void setActive(boolean active) {
         this.active = active;
     }
+    */
 }
